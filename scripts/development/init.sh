e95bbee8b6e526be1985fb89b2405b0369662dda
Anthony Simonelli <anthony.simonelli@lynch2.com>
2016-10-24 13:58:48 -0500

    Corrected mistakes and added "gulp" install since it is not available as a package for Ubuntu

--- scripts/development/init.sh
+++ scripts/development/init.sh
@@ -1,21 +1,23 @@
-
+#!/usr/bin/env bash
 # Run "npm install" (will rebuild "node_modules" folder)
 npm install
 
-#Run "gulp less" (will rebuild *.less files from resources/assets and put them into public/css)
+# Install and Run "gulp less" (will rebuild *.less files from resources/assets and put them into public/css)
+npm install gulp -g
+
 gulp less
 
 # Run "artisan config:clear" - clear config caches to be sure
 /usr/bin/env php artisan config:clear
 
-#Run "artisan view:clear" - clear view caches to be sure
-/usr/bin/env php
-
-#Run "artisan cache:clear" - clear all the other caches to be sure
+# Run "artisan view:clear" - clear view caches to be sure
 /usr/bin/env php artisan view:clear
 
-#Run "artisan clear-compiled" - clear artisan compiled classes
+# Run "artisan cache:clear" - clear all the other caches to be sure
+/usr/bin/env php artisan cache:clear
+
+# Run "artisan clear-compiled" - clear artisan compiled classes
 /usr/bin/env php artisan clear-compiled
 
-#Run "artisan optimize" - optimize framework
+# Run "artisan optimize" - optimize framework
 /usr/bin/env php artisan optimize
