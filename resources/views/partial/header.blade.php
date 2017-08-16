1c337f4f603081c4081beaacef744aa1bfa7df0d
Tony Kundmann <tony@lynch2.com>
2017-08-16 10:48:52 -0500

    Previous change not put in the proper code location.  Fixed that.

--- resources/views/partial/header.blade.php
+++ resources/views/partial/header.blade.php
@@ -6,14 +6,14 @@
             <img class="img-responsive" src="{{ secure_asset($site->logoUrl) }}">
         </a>
         @if (!$site->hasFeature(Feature::HIDE_TITLE))
-            <h3 class="companyName">{{ $site->title or '' }}</h3>
+            <h3 class="companyName {{ $site->code }}">{{ $site->title or '' }}</h3>
         @endif
     @endif
     @if (!isset($site) && isset($context) && ContextHelper::isAdminContext($context))
         <a class="logo" href="{{ route('main.home') }}">
             <img class="img-responsive" src="{{ secure_asset('/img/logo/logo-sipi.png') }}">
         </a>
-        <h3 class="companyName {{ $site->code }}">@lang('main.layout.admin')</h3>
+        <h3 class="companyName">@lang('main.layout.admin')</h3>
     @endif
     </div>
 </div>
