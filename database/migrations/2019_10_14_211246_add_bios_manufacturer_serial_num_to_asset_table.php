a35b6a3e340c1ed8eb4e0b88bfae5ebb55f3c89a
Tony Kundmann <tony@lynch2.com>
2019-10-14 16:47:26 -0500

    Updated migration to add new coliumn after a specific column in the table

--- database/migrations/2019_10_14_211246_add_bios_manufacturer_serial_num_to_asset_table.php
+++ database/migrations/2019_10_14_211246_add_bios_manufacturer_serial_num_to_asset_table.php
@@ -13,7 +13,7 @@ class AddBiosManufacturerSerialNumToAssetTable extends Migration
     public function up()
     {
         Schema::table('asset', function (Blueprint $table) {
-            $table->string('bios_manufacturer_serial_num', 50)->nullable()->default(null);
+            $table->string('bios_manufacturer_serial_num', 50)->after('manufacturer_serial_num')->nullable()->default(null);
         });
     }
 
