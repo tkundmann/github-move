ce4bfcaa0e61268e9a7111918445f603122fdc09
Tony Kundmann <tony@lynch2.com>
2019-12-06 10:57:53 -0600

    Created migrations for the purpose of changing the security_lock and security_lock_resolved columns from boolean to string.

---
+++ database/migrations/2019_12_05_232755_alter_bios_security_lock_fields_to_string_asset.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterBiosSecurityLockFieldsToStringAsset extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::table('asset', function (Blueprint $table) {
+            $table->string('security_lock', 5)->nullable()->default(null)->change();
+            $table->string('security_lock_resolved', 5)->nullable()->default(null)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('asset', function (Blueprint $table) {
+            $table->boolean('security_lock')->nullable()->default(null)->change();
+            $table->boolean('security_lock_resolved')->nullable()->default(null)->change();
+        });
+    }
+}
