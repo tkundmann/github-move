ef33151f8879799f3c2bd0a91dc7af0f464067c6
Tony Kundmann <tony@lynch2.com>
2020-02-06 15:34:08 -0600

    Created for the purpose of adding Address Book functionality.   Per AC task #513-3

---
+++ database/seeds/production/AirGasPickupRequestAddressSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Database\Seeds\Production;
+
+use App\Data\Models\Feature;
+use App\Data\Models\Site;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\DB;
+
+class AirGasPickupRequestAddressSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $site = Site::where('code', '=', 'airgas')->first();
+
+        $featureHasPickupRequestAddressBook = Feature::where('name', '=', Feature::PICKUP_REQUEST_ADDRESS_BOOK)->first();
+
+        $site->features()->attach([$featureHasPickupRequestAddressBook->id]);
+
+        $pickupRequestAddressBookConfiguration = array (
+            'site_address_book_label' => 'Site Address Book',
+            'new_site_text' => 'Provide to a new Site Name to create a new record.',
+            'new_site_address_book_label' => 'Site Name',
+            'change_text' => 'If you want to add this site to the address book, you must supply a new <b>Site Name</b>. Otherwise, the existing <b>Site Name</b> address record will be updated with the information submitted for this request.',
+            'allow_change' => true
+        );
+
+        $site->features()->updateExistingPivot($featureHasPickupRequestAddressBook->id, ['data' => serialize($pickupRequestAddressBookConfiguration)]);
+
+        DB::table('pickup_request_address')->insert([
+            [
+                'site_id' => $site->id,
+                'name' => 'Airgas',
+                'company_name' => 'Airgas',
+                'company_division' => null,
+                'contact_name' => Crypt::encrypt(''),
+                'contact_phone_number' => Crypt::encrypt(''),
+                'contact_address_1' => Crypt::encrypt('920 PINER ROAD'),
+                'contact_address_2' => null,
+                'contact_city' => 'SANTA ROSA',
+                'contact_state' => 'CA',
+                'contact_zip' => '95403',
+                'contact_country' => 'USA',
+                'contact_cell_number' => Crypt::encrypt(''),
+                'contact_email_address' => Crypt::encrypt(''),
+                'created_at' => Carbon::now()
+            ]
+        ]);
+
+    }
+}
