9e4427fe7676cef878b19811be64a52be66f733d
Tony Kundmann <tony@lynch2.com>
2020-06-22 15:14:44 -0500

    Created per adding additional_request_recipient_email_address field to Pickup Request form

---
+++ database/migrations/2020_06_22_201249_add_additional_request_recipient_email_address_to_pickuprequest.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAdditionalRequestRecipientEmailAddressToPickuprequest extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pickup_request', function (Blueprint $table) {
+            $table->integer('additional_request_recipient_email_address')->after('contact_email_address')->nullable()->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pickup_request', function (Blueprint $table) {
+            $table->dropColumn('additional_request_recipient_email_address');
+        });
+    }
+}
