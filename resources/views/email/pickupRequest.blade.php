58c300153cf070da1ebb002166844c02272a26b1
Tony Kundmann <tony@lynch2.com>
2020-06-23 13:34:36 -0500

    More DEV testing

--- resources/views/email/pickupRequest.blade.php
+++ resources/views/email/pickupRequest.blade.php
@@ -63,16 +63,11 @@
 			<td>@if($pickupRequest->contact_email_address) <a href="mailto:{{ $pickupRequest->contact_email_address }}">{{ $pickupRequest->contact_email_address }}</a> @endif</td>
         </tr>
 
+        @if(isset($pickupRequest->additional_request_recipient_email_address) && $pickupRequest->additional_request_recipient_email_address != '')
         <tr>
             <td>@lang('pickup_request.additional_request_recipient_email_address'):</td>
             <td>@if($pickupRequest->additional_request_recipient_email_address) <a href="mailto:{{ $pickupRequest->additional_request_recipient_email_address }}">{{ $pickupRequest->additional_request_recipient_email_address }}</a> @endif</td>
         </tr>
-
-        @if(isset($pickupRequest->additional_request_recipient_email_address) && $pickupRequest->additional_request_recipient_email_address != '')
-            <tr>
-                <td>@lang('pickup_request.additional_request_email_address'):</td>
-                <td>@if($pickupRequest->additional_request_recipient_email_address) <a href="mailto:{{ $pickupRequest->additional_request_recipient_email_address }}">{{ $pickupRequest->additional_request_recipient_email_address }}</a> @endif</td>
-            </tr>
         @endif
 
         @if($pickupRequestData['use_reference_number'])
