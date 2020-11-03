6f0f95ccc66a968d86baceeb2895355d2c1d967e
Tony Kundmann <tony@lynch2.com>
2020-11-02 21:10:46 -0600

    changed use of lessThan() to lt()

--- app/Controllers/Auth/AuthController.php
+++ app/Controllers/Auth/AuthController.php
@@ -129,7 +129,7 @@ class AuthController extends ContextController
             $now = Carbon::now();
             $daysTilExpiration = $password_expiry_at->diffInDays($now);
 
-            if($password_expiry_at->lessThan($now)){
+            if($password_expiry_at->lt($now)){
                 $request->session()->put('password_expired_id',$user->id);
                 auth()->logout();
                 return redirect()->route('password.expiration')->with('message', Lang::get('auth.password.change_expired_password'));
