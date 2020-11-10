c43dc3dd9231b71d30288c465992ce3f2a5fbec9
Tony Kundmann <tony@lynch2.com>
2020-11-10 10:08:18 -0600

    Bug fix in logic that determines if a password is expired

--- app/Controllers/Auth/AuthController.php
+++ app/Controllers/Auth/AuthController.php
@@ -129,7 +129,7 @@ class AuthController extends ContextController
             $now = Carbon::now();
             $daysTilExpiration = $password_expiry_at->diffInDays($now);
 
-            if($daysTilExpiration == 0){
+            if($daysTilExpiration <= 0){
                 $request->session()->put('password_expired_id',$user->id);
                 auth()->logout();
                 return redirect()->route('password.expiration')->with('message', Lang::get('auth.password.change_expired_password'));
