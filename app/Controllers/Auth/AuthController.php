15140d2bf5ccf8c5628b7c9771b9387ec2b395ec
Tony Kundmann <tony@lynch2.com>
2020-11-19 11:25:41 -0600

    Testing out custom get ip method on DEV portal.

--- app/Controllers/Auth/AuthController.php
+++ app/Controllers/Auth/AuthController.php
@@ -166,7 +166,25 @@ class AuthController extends ContextController
 
     protected function getThrottleKey(Request $request)
     {
-        return mb_strtolower($this->context . '|' . $request->input($this->loginUsername())) . '|' . $request->ip();
+        $key = mb_strtolower($this->context . '|' . $request->input($this->loginUsername()));
+        $ipAddress = $this->getIp();
+        if ($ipAddress) {
+            $key .= '|' . $ipAddress;
+        }
+        return $key;
+    }
+
+    public static function getIp(){
+        foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
+            if (array_key_exists($key, $_SERVER) === true){
+                foreach (explode(',', $_SERVER[$key]) as $ip){
+                    $ip = trim($ip); // just to be safe
+                    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false){
+                        return $ip;
+                    }
+                }
+            }
+        }
     }
 
     /**
