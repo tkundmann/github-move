76d2a0ca71fb1823f2331ef00b9575026a0aca45
Tony Kundmann <tony@lynch2.com>
2021-02-05 13:20:45 -0600

    Created Arctic Wolf Networks and Acxiom LLC pickup request forms and associated address books.

---
+++ database/seeds/production/AcxiomLLCPickupRequest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Database\Seeds\Production;
+
+use App\Data\Models\Feature;
+use App\Data\Models\Site;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AcxiomLLCPickupRequest extends Seeder
+{
+		/**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+		public function run() {
+
+		$featureHasPickupRequest = Feature::where('name', '=', Feature::HAS_PICKUP_REQUEST)->first();
+
+		$site = Site::where('code', '=', 'acxiomllc')->first();
+
+		$site->features()->attach([$featureHasPickupRequest->id]);
+
+		$pickupRequestConfiguration = array (
+			'password' => Hash::make('acx020521sar!'),
+			'title' => 'Sipi Asset Recovery/Acxiom LLC Pickup Request',
+			'use_company_division' => false,
+			'use_contact_section_title' => true,
+			'contact_section_title' => 'Site Location Pick-up Information',
+			'use_state_as_select' => false,
+			'use_country' => false,
+			'use_reference_number' => true,
+			'reference_number_label' => 'Customer Reference Number',
+			'use_alternative_piece_count_form' => false,
+			'use_crt_and_lcd_monitors' => true,
+			'use_preferred_pickup_date' => true,
+			'use_preferred_pickup_date_information' => false,
+			'use_lift_gate' => true,
+			'use_hardware_on_skids' => true,
+			'required_fields' =>
+			array (
+				'upload_equipment_list',
+				'company_name',
+				'contact_name',
+				'contact_address_1',
+				'contact_city',
+				'contact_phone_number',
+				'contact_email_address',
+				'reference_number',
+				'preferred_pickup_date',
+				'hardware_on_skids',
+				'units_located_near_dock',
+				'units_on_single_floor',
+				'is_lift_gate_needed',
+				'is_loading_dock_present',
+				'dock_appointment_required',
+				'assets_need_packaging'
+			),
+			'email_from' => 'saracxiomllc@sipiar.com',
+			'email_bcc' => 'saracxiomllc@sipiar.com;tony@lynch2.com;stan.henninger@acxiom.com',
+			'email_additional_bcc' =>
+			array (
+			),
+		);
+
+		$site->features()->updateExistingPivot($featureHasPickupRequest->id, ['data' => serialize($pickupRequestConfiguration)]);
+
+		$featureHasPickupRequestEquipList = Feature::where('name', '=', Feature::PICKUP_REQUEST_EQUIPMENT_LIST)->first();
+
+		$site->features()->attach([$featureHasPickupRequestEquipList->id]);
+
+		$pickupRequestEquipListConfiguration = array (
+			array (
+					'name' => 'Equipment List',
+					'filename' => 'AcxiomLLC_EquipmentList_US.xls',
+					'url' => 'https://belmont-sipi-assets.s3.amazonaws.com/acxiomllc/pickup_request/equipment_list_templates/AcxiomLLC_EquipmentList_US.xls',
+				)
+			);
+
+		$site->features()->updateExistingPivot($featureHasPickupRequestEquipList->id, ['data' => serialize($pickupRequestEquipListConfiguration)]);
+	}
+}
---
+++ database/seeds/production/AcxiomLLCPickupRequestAddressSeeder.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Database\Seeds\Production;
+
+use App\Data\Models\Feature;
+use App\Data\Models\Site;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\DB;
+
+class AcxiomLLCPickupRequestAddressSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $site = Site::where('code', '=', 'acxiomllc')->first();
+
+        $featureHasPickupRequestAddressBook = Feature::where('name', '=', Feature::PICKUP_REQUEST_ADDRESS_BOOK)->first();
+
+        $site->features()->attach([$featureHasPickupRequestAddressBook->id]);
+
+        $pickupRequestAddressBookConfiguration = array (
+            'site_address_book_label'     => 'Site Address Book',
+            'new_site_text'               => 'Provide to a new Site Name to create a new record.',
+            'new_site_address_book_label' => 'Site Name',
+            'change_text'                 => 'If you want to add this site to the address book, you must supply a new <b>Site Name</b>. Otherwise, the existing <b>Site Name</b> address record will be updated with the information submitted for this request.',
+            'allow_change'                => true,
+            'address_readonly'            => false,
+            'save_dock_info'              => false,
+        );
+
+        $site->features()->updateExistingPivot($featureHasPickupRequestAddressBook->id, ['data' => serialize($pickupRequestAddressBookConfiguration)]);
+
+        DB::table('pickup_request_address')->insert([
+            [
+                'site_id' => $site->id,
+                'name' => 'Acxiom - Conway',
+                'company_name' => 'Acxiom LLC',
+                'company_division' => null,
+                'contact_name' => Crypt::encrypt(''),
+                'contact_phone_number' => Crypt::encrypt(''),
+                'contact_address_1' => Crypt::encrypt('301 E Dave Ward Drive'),
+                'contact_address_2' => Crypt::encrypt(''),
+                'contact_city' => 'Conway',
+                'contact_state' => 'AK',
+                'contact_zip' => '72032',
+                'contact_country' => 'USA',
+                'contact_cell_number' => Crypt::encrypt(''),
+                'contact_email_address' => Crypt::encrypt(''),
+                'created_at' => Carbon::now()
+            ]
+        ]);
+
+    }
+}
