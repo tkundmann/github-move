3cb6a628aa74cfeebf2b479ba9d442a07fb0631e
Tony Kundmann <tony@lynch2.com>
2021-04-05 13:42:49 -0500

    Created for updating White Cap Pickup Request configuration settings

---
+++ database/seeds/production/WhiteCapPickupRequestUpdateConfig.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Database\Seeds\Production;
+
+use App\Data\Models\Feature;
+use App\Data\Models\Site;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class WhiteCapPickupRequestUpdateConfig extends Seeder
+{
+		/**
+		* Run the database seeds.
+		*
+		* @return void
+		*/
+		public function run() {
+
+			$site = Site::where('code', '=', 'whitecap')->first();
+
+			$featureHasPickupRequest = Feature::where('name', '=', Feature::HAS_PICKUP_REQUEST)->first();
+
+			$pickupRequestConfiguration = array (
+				'password' => Hash::make('vS^RJCJf#92xf/dK'),
+				'title' => 'Sipi Asset Recovery/White Cap Pickup Request',
+				'use_company_division' => false,
+				'use_contact_section_title' => true,
+				'contact_section_title' => 'Site Location Pick-up Information',
+				'use_state_as_select' => false,
+				'use_country' => false,
+				'use_reference_number' => true,
+				'reference_number_label' => 'Customer Reference Number',
+				'use_alternative_piece_count_form' => false,
+				'use_crt_and_lcd_monitors' => true,
+				'use_preferred_pickup_date' => true,
+				'use_preferred_pickup_date_information' => false,
+				'use_lift_gate' => true,
+				'use_hardware_on_skids' => true,
+				'required_fields' =>
+				array (
+					'upload_equipment_list',
+					'company_name',
+					'contact_name',
+					'contact_address_1',
+					'contact_city',
+					'contact_phone_number',
+					'contact_email_address',
+					'reference_number',
+					'preferred_pickup_date',
+					'hardware_on_skids',
+					'units_located_near_dock',
+					'units_on_single_floor',
+					'is_lift_gate_needed',
+					'is_loading_dock_present',
+					'dock_appointment_required',
+					'assets_need_packaging'
+				),
+				'email_from' => 'sarwhitecap@sipiar.com',
+				'email_bcc' => 'sarwhitecap@sipiar.com;tony@lynch2.com;jamie.michalsen@insight.com;dana.smith@insight.com;luke.joyce@insight.com;assets@whitecap.com',
+				'email_additional_bcc' =>
+				array (
+				),
+			);
+
+			$site->features()->updateExistingPivot($featureHasPickupRequest->id, ['data' => serialize($pickupRequestConfiguration)]);
+		}
+}
